f14c560e28d64f5ea8b712da06816af272a73ebe
Andrey Lebedev <alebedev424@gmail.com>
2024-01-17 11:13:17 +0300

    feat: creat simple todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import styles from './App.module.scss';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState('')
+
+  const addTodo = () => {
+    if (text) {
+      setTodos([
+        ...todos,
+        {
+          id: new Date().toISOString(),
+          text,
+          completed: false,
+        }
+      ])
+      setText('')
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <label>
+        <input value={text} onChange={(e) => setText(e.target.value)}></input>
+        <button className={styles.button} onClick={addTodo}>Add</button>
+      </label>
+
+      <ul className={`${styles.list} list`}>
+        {todos.map(todo =>
+          <li key={todo.id}>
+            <input type='checkbox' />
+            <span className={styles.text}>{todo.text}</span>
+            <span className={styles.delete}>&times;</span>
+          </li>)}
+      </ul>
+
     </div>
   );
 }
